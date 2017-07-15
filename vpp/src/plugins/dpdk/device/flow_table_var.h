91ed2e4db9e1976ccaae8770abfb46ea52c70e97
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2017-07-15 08:55:29 +0200

    BW fair_drop

---
+++ vpp/src/plugins/dpdk/device/flow_table_var.h
@@ -0,0 +1,12 @@
+#include <dpdk/device/flow_table.h>
+flowcount_t *  nodet[TABLESIZE] ;
+activelist_t * head_af;
+activelist_t * tail_af;
+flowcount_t *  head ;
+flowcount_t *  previousnode;
+flowcount_t *  tail;
+int numflows;
+u32 r_qtotal;
+u32 nbl;
+u64 t = 0;
+u64 old_t;
