be2969982eb97ad563cd0a0ee76eeaf1c0d84663
Leonardo Linguaglossa <theleos88@gmail.com>
2017-10-23 10:13:49 +0200

    New deadline.h file

--- vpp/src/vlib/threads.c
+++ vpp/src/vlib/threads.c
@@ -29,63 +29,7 @@ DECLARE_CJ_GLOBAL_LOG;
 #define SCHED_ENABLE 1
 
 #ifdef SCHED_ENABLE
-#include <sched.h>
-#include <sys/syscall.h>
-#include <errno.h>
-#define gettid() syscall(__NR_gettid)
-
- #define SCHED_DEADLINE	6
-
- /* XXX use the proper syscall numbers */
- #ifdef __x86_64__
- #define __NR_sched_setattr		314
- #define __NR_sched_getattr		315
- #endif
-
- #ifdef __i386__
- #define __NR_sched_setattr		351
- #define __NR_sched_getattr		352
- #endif
-
- #ifdef __arm__
- #define __NR_sched_setattr		380
- #define __NR_sched_getattr		381
- #endif
-
- static volatile int done;
-
- struct sched_attr {
-	u32 size;
-
-	u32 sched_policy;
-	u64 sched_flags;
-
-	/* SCHED_NORMAL, SCHED_BATCH */
-	int sched_nice;
-
-	/* SCHED_FIFO, SCHED_RR */
-	u32 sched_priority;
-
-	/* SCHED_DEADLINE (nsec) */
-	u64 sched_runtime;
-	u64 sched_deadline;
-	u64 sched_period;
- };
-
- int sched_setattr(pid_t pid,
-		  const struct sched_attr *attr,
-		  unsigned int flags)
- {
-	return syscall(__NR_sched_setattr, pid, attr, flags);
- }
-
- int sched_getattr(pid_t pid,
-		  struct sched_attr *attr,
-		  unsigned int size,
-		  unsigned int flags)
- {
-	return syscall(__NR_sched_getattr, pid, attr, size, flags);
- }
+#include "vlib/deadline.h"
 #endif
 
 
