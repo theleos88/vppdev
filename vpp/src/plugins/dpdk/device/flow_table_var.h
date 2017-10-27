837b299890d77a2a3b06b47fc7a86d6b7280f03a
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2017-10-27 16:38:31 +0200

    update

--- vpp/src/plugins/dpdk/device/flow_table_var.h
+++ vpp/src/plugins/dpdk/device/flow_table_var.h
@@ -1,9 +1,5 @@
 #include <plugins/dpdk/device/flow_table_cpu.h>
-<<<<<<< HEAD
-flowcount_t *  nodet[2][256] ;
-=======
 flowcount_t *  nodet[256][2] ;
->>>>>>> cf739c647ec64f4329e344c8fefcd6885216366e
 activelist_t * head_af[2];
 activelist_t * tail_af[2];
 flowcount_t *  head[2] ;
